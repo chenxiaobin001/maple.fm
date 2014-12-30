9a212958b55b9e6f75edd620a872d2d05414636e
cxb <lakayiiii@gmail.com>
2014-12-29 23:55:32 -0500

    loading screen

---
+++ src/com/example/maplefreemarket/LoadingScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.maplefreemarket;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
+public class LoadingScreenActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		final Intent i = new Intent(this, HomeActivity.class);
+		setContentView(R.layout.loading_screen);
+
+		CountDownTimer timer = new CountDownTimer(3000, 1000) {
+
+			@Override
+			public void onTick(long millisUntilFinished) {
+
+			}
+
+			@Override
+			public void onFinish() {
+				startActivity(i);
+				finish();
+			}
+
+		};
+		timer.start();
+	}
+
+}
