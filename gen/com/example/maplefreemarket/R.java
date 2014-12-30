a8084113572ee24159e09aa38187b89d332cea18
cxb <lakayiiii@gmail.com>
2014-12-30 07:44:36 -0500

    1

--- gen/com/example/maplefreemarket/R.java
+++ gen/com/example/maplefreemarket/R.java
@@ -1181,7 +1181,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050044;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1198,6 +1198,7 @@ containing a value of this type.
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int filterButton=0x7f05003e;
+        public static final int firstLine=0x7f050042;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1206,7 +1207,7 @@ containing a value of this type.
         public static final int linearLayout1=0x7f05003c;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int loading_screen_layout=0x7f050041;
+        public static final int loading_screen_layout=0x7f050043;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1224,8 +1225,9 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
-        public static final int serverImage=0x7f050042;
-        public static final int serverName=0x7f050043;
+        public static final int secondLine=0x7f050041;
+        public static final int serverImage=0x7f050044;
+        public static final int serverName=0x7f050045;
         public static final int serverSpinner=0x7f05003d;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
@@ -1291,9 +1293,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int loading_screen=0x7f030019;
-        public static final int row=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int item_row=0x7f030019;
+        public static final int loading_screen=0x7f03001a;
+        public static final int row=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
@@ -1340,10 +1343,15 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
+        public static final int api_item=0x7f0a0013;
+        public static final int api_list_items=0x7f0a0016;
+        public static final int api_pricecheck=0x7f0a0015;
+        public static final int api_rankings=0x7f0a0014;
+        public static final int api_search=0x7f0a0012;
         public static final int app_name=0x7f0a000d;
-        public static final int filter_button=0x7f0a0012;
+        public static final int filter_button=0x7f0a0017;
         public static final int hello_world=0x7f0a000e;
-        public static final int search_button=0x7f0a0011;
+        public static final int refresh_button=0x7f0a0011;
         public static final int search_text=0x7f0a0010;
     }
     public static final class style {
